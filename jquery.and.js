f2079ac3e0beeec19bd1433ff95e082386c114a9
Mikael Karon <mikael@karon.se>
2017-06-15 15:26:11 +0200

    fix: UMD fixes
    Fix UMD wrapper to for static analysis

--- jquery.and.js
+++ jquery.and.js
@@ -1,12 +1,12 @@
-(function (modules, root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(modules, factory);
+    define([], factory);
   } else if (typeof module === "object" && module.exports) {
     module.exports = factory.call(root);
   } else {
     root["mu-jquery-and/jquery.and"] = factory.call(root);
   }
-})([], this, function () {
+})(this, function () {
   function and($, element, match) {
     return $(match).length !== 0;
   }
